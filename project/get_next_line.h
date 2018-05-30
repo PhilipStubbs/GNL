c3005bdb8154466d5fe4079feaa2b6e7218e5b95
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-05-30 14:59:11 +0200

    base setup

---
+++ project/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pstubbs <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/30 10:19:09 by pstubbs           #+#    #+#             */
+/*   Updated: 2018/05/30 10:19:14 by pstubbs          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft.h"
+
+# define BUFF_SIZE 1024
+int	get_next_line(const int fd, char **line);
+
+#endif
