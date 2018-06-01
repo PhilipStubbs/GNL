918bdcb62d7e0e24a70bb23baea570238d877139
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-06-01 14:07:03 +0200

    proto added

--- project/get_next_line.c
+++ project/get_next_line.c
@@ -12,6 +12,8 @@
 
 #include "get_next_line.h"
 
+#include <stdio.h>
+
 // int	get_next_line(const int fd, char **line)
 // {
 // 	int ret;
@@ -20,36 +22,49 @@
 
 
 
-int    main(int argc, char **argv)
+int    ft_GNLProto()
 {
 
-	char buff[60000];
-	int fd;
+	char buff[BUFF_SIZE +1];
+	static int fd;
 	int byter;
 	int i;
-	static char	*ret;
-	static int j;
-	static int e;
+	char	*ret;
+	// static int j;
+	// static int e;
 
 	i = 0;
-	j = 0;
-	ret = (char*)ft_memalloc(60000 * sizeof(char));
-	fd = open("test.txt", O_RDONLY);
-	while ((byter = read(fd, buff, 10)) > 0)
+	ft_bzero(buff, BUFF_SIZE + 1);
+	ret = (char*)ft_memalloc(100 * sizeof(char));
+	if(!ret)
+		return(0);
+	if(!fd)
+		fd = open("test.txt", O_RDONLY);
+	while ((byter = read(fd, buff, BUFF_SIZE )) > 0)
 	{	
+		write(1, "x" ,1 );
 		i +=byter;
 		ft_strcat(ret, buff);
-		// if (ft_strchr(buff, '\n'))
-		// {
-		// 	break;
-		// }		
+		if (ft_strchr(buff, '\n' || ft_strchr(buff, EOF ))
+		{
+			write(1, "e" ,1 );
+			break;
+		}
+		ft_bzero(buff, BUFF_SIZE);
 	}
-		// (*ft_strchr(ret, '\n')) = '\0';
-		while (ret[j] != '\n')
-			j++;
 		(*ft_strchr(ret, '\n')) = '\0';
-		printf("%d, [%s]\n" ,i,ret);
 
+		printf("%d, [%s]\n" ,i,ret);
+		free(ret);
 
 	return (0);
+}
+
+int	main()
+{
+	ft_GNLProto();
+	ft_GNLProto();
+	ft_GNLProto();
+	ft_GNLProto();
+	ft_GNLProto();
 }
--- project/get_next_line.h
+++ project/get_next_line.h
@@ -15,7 +15,7 @@
 # include "libft/libft.h"
 # include <fcntl.h>
 
-# define BUFF_SIZE 1024
+# define BUFF_SIZE 10
 int	get_next_line(const int fd, char **line);
 
 #endif
